4e4ce29483804f6618ae7efd2bb6ac9a5125f5c2
xurong <xurong@forchange.tech>
2020-06-24 23:25:18 +0800

    Feat:比较版本号

---
+++ leetcode-swift/leetcode-swift/165.比较版本号.swift
@@ -0,0 +1,71 @@
+//
+//  165.比较版本号.swift
+//  leetcode-swift
+//
+//  Created by 徐荣 on 2020/6/24.
+//  Copyright © 2020 xurong. All rights reserved.
+//
+
+import Foundation
+
+/*
+ 比较两个版本号 version1 和 version2。
+ 如果 version1 > version2 返回 1，如果 version1 < version2 返回 -1， 除此之外返回 0。
+
+ 你可以假设版本字符串非空，并且只包含数字和 . 字符。
+  . 字符不代表小数点，而是用于分隔数字序列。
+ 
+ 例如，2.5 不是“两个半”，也不是“差一半到三”，而是第二版中的第五个小版本。
+ 你可以假设版本号的每一级的默认修订版号为 0。例如，版本号 3.4 的第一级（大版本）和第二级（小版本）修订号分别为 3 和 4。其第三级和第四级修订号均为 0。
+ 
+ 示例 1:
+ 输入: version1 = "0.1", version2 = "1.1"
+ 输出: -1
+ 
+ 示例 2:
+ 输入: version1 = "1.0.1", version2 = "1"
+ 输出: 1
+ 
+ 示例 3:
+ 输入: version1 = "7.5.2.4", version2 = "7.5.3"
+ 输出: -1
+ 
+ 示例 4：
+ 输入：version1 = "1.01", version2 = "1.001"
+ 输出：0
+ 解释：忽略前导零，“01” 和 “001” 表示相同的数字 “1”。
+ 
+ 示例 5：
+ 输入：version1 = "1.0", version2 = "1.0.0"
+ 输出：0
+ 解释：version1 没有第三级修订号，这意味着它的第三级修订号默认为 “0”。
+  
+
+ 提示：
+ 版本字符串由以点 （.） 分隔的数字字符串组成。这个数字字符串可能有前导零。
+ 版本字符串不以点开始或结束，并且其中不会有两个连续的点。
+ */
+
+extension Solution {
+    func compareVersion(_ version1: String, _ version2: String) -> Int {
+        var arr1 = version1.components(separatedBy: ".")
+        var arr2 = version2.components(separatedBy: ".")
+        let count = max(arr1.count, arr2.count)
+        while arr1.count != count {
+            arr1.append("0")
+        }
+        while arr2.count != count {
+            arr2.append("0")
+        }
+        for i in 0..<count {
+            let a1 = Int(arr1[i]) ?? 0
+            let a2 = Int(arr2[i]) ?? 0
+            if a1 > a2 {
+                return 1
+            } else if a1 < a2 {
+                return -1
+            }
+        }
+        return 0
+    }
+}
