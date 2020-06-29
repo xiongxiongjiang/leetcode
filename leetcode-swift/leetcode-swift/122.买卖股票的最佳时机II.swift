c22873bd3404f83ea59608659401f55b891d075e
xurong <xurong@forchange.tech>
2020-06-29 18:58:38 +0800

    Feat:扑克牌中的顺子

--- leetcode-swift/leetcode-swift/122.买卖股票的最佳时机II.swift
+++ leetcode-swift/leetcode-swift/122.买卖股票的最佳时机II.swift
@@ -10,7 +10,7 @@ import Foundation
 
 /*
  给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
- 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+ 设计一个算法来计算你所能获取的最大利润。你可以  尽可能地完成更多  的交易（多次买卖一支股票）。
  注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
 
  示例 1:
@@ -46,6 +46,7 @@ extension Solution {
         var profit = 0
         for i in 1..<prices.count {
             let tmp = prices[i] - prices[i - 1]
+            //只有赚的情况才买入
             profit = tmp > 0 ? profit + tmp : profit
         }
         return profit
---
+++ leetcode-swift/leetcode-swift/剑指Offer61.扑克牌中的顺子.swift
@@ -0,0 +1,46 @@
+//
+//  剑指Offer61.扑克牌中的顺子.swift
+//  leetcode-swift
+//
+//  Created by 徐荣 on 2020/6/29.
+//  Copyright © 2020 xurong. All rights reserved.
+//
+
+import Foundation
+
+/*
+ 从扑克牌中随机抽5张牌，判断是不是一个顺子，即这5张牌是不是连续的。2～10为数字本身，A为1，J为11，Q为12，K为13，而大、小王为 0 ，可以看成任意数字。A 不能视为 14。
+
+ 示例 1:
+ 输入: [1,2,3,4,5]
+ 输出: True
+
+ 示例 2:
+ 输入: [0,0,1,2,5]
+ 输出: True
+
+ 限制：
+ 数组长度为 5
+ 数组的数取值为 [0, 13] .
+ */
+
+extension Solution {
+    func isStraight(_ nums: [Int]) -> Bool {
+        let nums = nums.sorted()
+        var zeroCount = 0
+        for i in 0..<nums.count-1 {
+            if nums[i] == 0 {
+                zeroCount += 1
+            } else if nums[i] == nums[i+1] {
+                return false
+            }
+        }
+        //nums[zeroCount]其实就是最小牌
+        return nums.last! - nums[zeroCount] < 5
+    }
+}
+
+func test0ffer61(s: Solution) {
+    let num = [0, 1, 0, 2, 5]
+    print(s.isStraight(num))
+}
